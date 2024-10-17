2c5dfdf77a0de27c11bd456818b54b94ba316d6a
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-10-17 23:06:11 +0600

    db connect

---
+++ service/mongo.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+export async function dbConnect() {
+    try {
+        const conn = await mongoose.connect(String(process.env.MONGODB_CONNECTION_STRING));
+        console.log("MongoDB connected");
+        return conn;
+
+
+    } catch (err) {
+        console.error(err);
+    }
+}
